7ddf6dd423156fa6b53adc0d4c1ab7d1d07bdbe2
JesudianChallapalli <challap@uwindsor.ca>
2020-07-23 02:39:52 -0400

    added the content into the groovy file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,23 +1,28 @@
-Pipleline {
-      agent any
-      tools {
-               jdk "java-1.8"
-	     } 
-	   Stages {
-	           stage ('SCM checkout') {
-		    steps {
-		            git url:'https://github.com/jesudian/realtime-example-cicd.git'
-			    }
-			}
-		   stage ('archiving artifacts') {
-		    steps {
-		            archiveArtifacts '**/*.html'
-			    }
-			 }
-		   stage ('transfer artifacts') {
-		   steps {
-		           sshPublisher (Publishers:[sshPublisherDesc(Configname:'webserver',transfers:[sshTransfer(excludes:'',execCommand: '',execTimeout:1200000,flatten=true,makeEmptyDirs:false,useWorkspaceInPromotion:false,verbose:true)])
-			   }
-			 }
-			}
-	 }
+pipeline {
+    agent any
+      
+        tools {
+              jdk "Java-1.8"
+               }
+               
+       stages {
+            stage('SCM checkout') {
+                  steps {
+                        git url: 'give your github repository url'
+                        }
+             }
+             
+             stage('archiving artifacts') {
+                  steps {
+                          archiveArtifacts '**/*.html'
+                        }
+              }
+              
+              stage('transfer artifacts') {
+                    steps {
+                          sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(excludes: '', execCommand: '', execTimeout: 120000, flatten: true, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/var/www/html', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: true)])
+                          }
+              }
+       }
+}
+
